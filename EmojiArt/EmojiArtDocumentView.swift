741e2a3e48d251147db044344a8d705f32fda635
Pablo Cornejo <pcornejo@icloud.com>
2020-06-16 18:17:05 -0400

    Rename ContentView to EmojiArtDocumentView

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiArtDocumentView.swift
 //  EmojiArt
 //
 //  Created by Pablo Cornejo on 6/16/20.
@@ -8,14 +8,8 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct EmojiArtDocumentView: View {
     var body: some View {
         Text("Hello, World!")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
