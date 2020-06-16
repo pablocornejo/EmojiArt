a8eeeca4b3bb8bf3f5cf49a6542e9366d64d8dae
Pablo Cornejo <pcornejo@icloud.com>
2020-06-16 18:15:01 -0400

    Add EmojiArtDocument file

---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Pablo Cornejo on 6/16/20.
+//  Copyright © 2020 Pablo Cornejo Pierola. All rights reserved.
+//
+
+import SwiftUI
+
+
+/// The emoji art document view model.
+class EmojiArtDocument: ObservableObject {
+    
+}
