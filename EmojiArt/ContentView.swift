e9601260aa7547391283aff3c65d71ef716ac871
Pablo Cornejo <pcornejo@icloud.com>
2020-06-16 18:05:44 -0400

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Pablo Cornejo on 6/16/20.
+//  Copyright © 2020 Pablo Cornejo Pierola. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
