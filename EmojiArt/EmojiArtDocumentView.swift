8e7d621f39b5d2f55dcaf5dba53c70f6bb61c395
Pablo Cornejo <pcornejo@icloud.com>
2020-06-16 20:29:26 -0400

    Implement basics of EmofiArtDocumentView

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -9,7 +9,25 @@
 import SwiftUI
 
 struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self) { emoji in
+                        Text(emoji)
+                            .font(.system(size: self.defaultEmojiSize))
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            Rectangle()
+                .foregroundColor(.yellow)
+                .edgesIgnoringSafeArea([.horizontal, .bottom])
+        }
     }
+    
+    private let defaultEmojiSize: CGFloat = 40
 }
