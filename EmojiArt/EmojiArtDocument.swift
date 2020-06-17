f753e79f64bcfc147f4fc40cd70377469850667c
Pablo Cornejo <pcornejo@icloud.com>
2020-06-16 20:20:03 -0400

    Add properties and intents to view model

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -11,5 +11,30 @@ import SwiftUI
 
 /// The emoji art document view model.
 class EmojiArtDocument: ObservableObject {
+    static let palette: String = "🍄🌜⚡️💥🌏"
     
+    @Published private var emojiArt: EmojiArt = EmojiArt()
+    
+    // MARK: - Intents
+    
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
+    
+    func setBackgroundURL(_ url: URL?) {
+        emojiArt.backgroundURL = url?.imageURL
+    }
 }
