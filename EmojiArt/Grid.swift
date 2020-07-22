3bf4b2493939651683e8a4f91dccfe5aba77fd1c
Pablo Cornejo <pcornejo@icloud.com>
2020-07-22 05:35:09 -0400

    Add Grid view

---
+++ EmojiArt/Grid.swift
@@ -0,0 +1,46 @@
+//
+//  Grid.swift
+//  Memorize
+//
+//  Created by Pablo Cornejo on 5/26/20.
+//  Copyright © 2020 Pablo Cornejo Pierola. All rights reserved.
+//
+
+import SwiftUI
+
+struct Grid<Item, ID, ItemView>: View where ID: Hashable, ItemView: View {
+    private var items: [Item]
+    private var viewForItem: (Item) -> ItemView
+    private let id: KeyPath<Item, ID>
+    
+    init(_ items: [Item], id: KeyPath<Item, ID>, viewForItem: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.id = id
+        self.viewForItem = viewForItem
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            self.body(for: GridLayout(itemCount: self.items.count, in: geometry.size))
+        }
+    }
+    
+    private func body(for layout: GridLayout) -> some View {
+        ForEach(items, id: id) { item in
+            self.body(for: item, in: layout)
+        }
+    }
+    
+    private func body(for item: Item, in layout: GridLayout) -> some View {
+        let index = items.firstIndex { item[keyPath: id] == $0[keyPath: id] }!
+        return viewForItem(item)
+            .frame(width: layout.itemSize.width, height: layout.itemSize.height)
+            .position(layout.location(ofItemAt: index))
+    }
+}
+
+extension Grid where Item: Identifiable, ID == Item.ID {
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
+        self.init(items, id: \.id, viewForItem: viewForItem)
+    }
+}
