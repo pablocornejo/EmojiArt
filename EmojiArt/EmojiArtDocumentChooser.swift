1892c7e9101aaabd5b40a3c49a0955a14b550516
Pablo Cornejo <pcornejo@icloud.com>
2020-07-24 07:35:58 -0400

    Add ability to rename documents

--- EmojiArt/EmojiArtDocumentChooser.swift
+++ EmojiArt/EmojiArtDocumentChooser.swift
@@ -10,13 +10,16 @@ import SwiftUI
 
 struct EmojiArtDocumentChooser: View {
     @EnvironmentObject var store: EmojiArtDocumentStore // could be @ObservedObject too
+    @State private var editMode: EditMode = .inactive
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(store.documents) { document in
                     NavigationLink(destination: EmojiArtDocumentView(document: document).navigationBarTitle(self.store.name(for: document))) {
-                        Text(self.store.name(for: document))
+                        EditableText(self.store.name(for: document), isEditing: self.editMode.isEditing) { name in
+                            self.store.setName(name, for: document)
+                        }
                     }
                 }
                 .onDelete { indexSet in
@@ -32,6 +35,7 @@ struct EmojiArtDocumentChooser: View {
                 },
                 trailing: EditButton()
             )
+            .environment(\.editMode, $editMode)
         }
     }
 }
